fb972af870422872dcda59d4e70a63fb395953db
GitHub Action <action@github.com>
2025-01-05 00:43:16 +0000

    Template cleanup

--- src/main/kotlin/com/github/sofiacf/praatlanguageplugin/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/sofiacf/praatlanguageplugin/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.sofiacf.praatlanguageplugin.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
